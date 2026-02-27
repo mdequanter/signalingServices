6ccbf3598b09e9a29f891601b8f4aa3503744ada
Maarten Dequanter <74420584+mdequanter@users.noreply.github.com>
2026-02-27 08:10:12 +0100

    Update segmentVideoServer.py

--- segmentVideoServer.py
+++ segmentVideoServer.py
@@ -188,6 +188,8 @@ async def receive_and_infer():
                 next_save_at = now + SAVE_INTERVAL_SEC
                 saved_this_frame = True
 
+            print (f"Last model used: {lastmodel}, latency: {lastlatency} ms, frame_id: {frame_id}, longitude: {longitude}, latitude: {latitude}")
+
             heading = compute_heading(frame, model=lastmodel)
 
             if (lastmodel == 2):
